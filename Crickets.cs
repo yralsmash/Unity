83f544e4b89d0280cb81ed87516ae7466ce29e8f
Круглов Сергей <66778309+yralsmash@users.noreply.github.com>
2024-12-25 00:53:50 +0800

    Add files via upload

---
+++ Crickets.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crickets : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        //в консоли каждый кадр выводится слово «crickets».
+        print("crickets");
+    }
+}
---
+++ Destroy.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    void Start()
+    {
+        //для уничтожения игрового объекта в начале игры
+        Destroy(gameObject);
+    }
+    void Update()
+    {
+        
+    }
+}
